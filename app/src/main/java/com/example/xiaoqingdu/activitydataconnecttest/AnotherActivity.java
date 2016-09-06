016918df878942622526c1e2537c2ace338ab0f2
xiaoqingdu <1329965378@qq.com>
2016-09-06 08:27:05 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xiaoqingdu/activitydataconnecttest/AnotherActivity.java
@@ -0,0 +1,42 @@
+package com.example.xiaoqingdu.activitydataconnecttest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+        Intent intent=getIntent();
+        String name=intent.getStringExtra("name");
+        Integer age=intent.getIntExtra("age", 20);
+        Toast.makeText(this,name+"  "+age,Toast.LENGTH_LONG).show();
+        Button button=(Button)findViewById(R.id.buttonback);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=getIntent();
+                String name=intent.getStringExtra("name");
+                Integer age=intent.getIntExtra("age", 20);
+                intent.putExtra("result","姓名:"+name+" 年龄"+age);
+                setResult(0,intent);
+                finish();
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==0 && resultCode==0){
+            String str=data.getStringExtra("result");
+            Toast.makeText(this,str,Toast.LENGTH_LONG).show();
+        }
+
+    }
+}
